23c76c9400289a8e27e564102a3fc03d458a8aeb
BrandonSev <seveste.brandon@gmail.com>
2022-04-13 13:53:46 +0200

    fix erreur syntaxe model likes

--- src/models/likes.models.js
+++ src/models/likes.models.js
@@ -2,32 +2,32 @@ const { connection } = require("../../db-connection");
 
 class Like {
   static findMany() {
-    const sql = "SELECT * FROM like";
+    const sql = "SELECT * FROM `like`";
     return connection.promise().query(sql);
   }
 
   static findOneById(id) {
-    const sql = "SELECT * FROM like WHERE id = ?";
+    const sql = "SELECT * FROM `like` WHERE id = ?";
     return connection.promise().query(sql, [id]);
   }
 
   static createOne(like) {
-    const sql = "INSERT INTO like SET ?";
+    const sql = "INSERT INTO `like` SET ?";
     return connection.promise().query(sql, [like]);
   }
 
   static updateOneById(like, id) {
-    const sql = "UPDATE like SET ? WHERE id = ?";
+    const sql = "UPDATE `like` SET ? WHERE id = ?";
     return connection.promise().query(sql, [like, id]);
   }
 
   static deleteOneById(id) {
-    const sql = "DELETE FROM like WHERE id = ?";
+    const sql = "DELETE FROM `like` WHERE id = ?";
     return connection.promise().query(sql, [id]);
   }
 
   static findOneByName(name) {
-    const sql = "SELECT * FROM like WHERE name=?";
+    const sql = "SELECT * FROM `like` WHERE name=?";
     return connection.promise().query(sql, [name]);
   }
 }
